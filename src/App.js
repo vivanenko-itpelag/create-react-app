e1ada9063f2c2a418c967d250ac6778e66dcadef
vivanenko <v.ivanenko@itpelag.com>
2023-03-25 15:26:20 +0300

    change from cookie to headers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import axios from 'axios';
-import JsCookie from 'js-cookie';
-import { JSEncrypt } from "jsencrypt";
+import { JSEncrypt } from 'jsencrypt';
 
 function App() {
-    const host = 'https://passmgr.itpelag.com/api'
+    const BACKEND_DOMAIN = 'passmgr.itpelag.com'; //@todo to config
+    const host = 'https://' + BACKEND_DOMAIN + '/api';
+    
     let username = '';
+    
     const axiosInstance = axios.create({
         withCredentials: true,
         baseURL: host,
         credentials: 'include',
-    })
+    });
     
-    function changeUsername(e)
-    {
+    function changeUsername(e) {
         username = e.target.value;
     }
     
-    function ping(e)
-    {
+    function ping(e) {
         axiosInstance.post('/auth/ping')
         .then(function (response) {
             console.log(response);
@@ -31,8 +31,7 @@ function App() {
         });
     }
     
-    function createKey(e)
-    {
+    function createKey(e) {
         axiosInstance.post('/auth/create-session', {
             sessionKey: localStorage.getItem('serverSessionKey'),
             publicKey: localStorage.getItem('serverPublicKey'),
@@ -45,12 +44,11 @@ function App() {
         });
     }
     
-    function generateSecret(e)
-    {
+    function generateSecret(e) {
         axiosInstance.post('/auth/generate-secret', {
             sessionKey: localStorage.getItem('serverSessionKey'),
             publicKey: localStorage.getItem('clientPublicKey'),
-            username: username
+            username: username,
         }).then(function (response) {
             console.log(response);
             let data = response.data.data;
@@ -62,8 +60,7 @@ function App() {
         });
     }
     
-    function decodeSecret()
-    {
+    function decodeSecret() {
         let encodedSecret = localStorage.getItem('encodedSecret');
         let decodedSecret = atob(encodedSecret);
         console.log(decodedSecret);
@@ -72,7 +69,7 @@ function App() {
         let decrypt = new JSEncrypt({
             default_key_size: 2048,
             log: true,
-            default_public_exponent: '10000000000000001'
+            default_public_exponent: '10000000000000001',
         });
         decrypt.setPrivateKey(clientPrivateKey);
         let decryptedValue = decrypt.decrypt(decodedSecret);
@@ -81,31 +78,34 @@ function App() {
         return decryptedValue;
     }
     
-    function signIn(e)
-    {
+    function signIn(e) {
         // let secretDecoded = decodeSecret();
         let secretDecoded = localStorage.getItem('decodedDigit');
-    
+        
         axiosInstance.post('/auth/sign-in', {
             sessionKey: localStorage.getItem('serverSessionKey'),
             publicKey: localStorage.getItem('clientPublicKey'),
             username: username,
-            digit: secretDecoded
+            digit: secretDecoded,
         })
         .then(function (response) {
             console.log(response);
             let data = response.data.data;
-            JsCookie.set('auth', data.authToken);
-            JsCookie.set('sessionKey', data.sessionToken);
+            localStorage.setItem('auth', data.authToken);
+            localStorage.setItem('sessionKey', data.sessionToken);
         }).catch(function (response) {
             showError(response);
             console.log(response);
         });
     }
     
-    function testSomeAuthMethod(e)
-    {
-        axiosInstance.get('/user/me')
+    function testSomeAuthMethod(e) {
+        axiosInstance.get('/user/me', {
+            headers: {
+                "X-Auth-Token": localStorage.getItem('auth'),
+                "X-Session-Token": localStorage.getItem('sessionKey'),
+            }
+        })
         .then(function (response) {
             console.log(response);
         }).catch(function (response) {
@@ -114,43 +114,40 @@ function App() {
         });
     }
     
-    function showError(response)
-    {
+    function showError(response) {
         let responseData = response.response;
         alert(responseData.data.data.message + '. Code: ' + responseData.status);
     }
     
-    function readPublicKey(input)
-    {
+    function readPublicKey(input) {
         let reader = new FileReader();
-        reader.readAsText(input.target.files[0]);
-        reader.onload = function() {
+        reader.readAsText(input.target.files[ 0 ]);
+        reader.onload = function () {
             console.log(reader.result);
             localStorage.setItem('clientPublicKey', reader.result);
-        }
+        };
     }
     
-    function readPrivateKey(input)
-    {
+    function readPrivateKey(input) {
         let reader = new FileReader();
-        reader.readAsText(input.target.files[0]);
-        reader.onload = function() {
+        reader.readAsText(input.target.files[ 0 ]);
+        reader.onload = function () {
             console.log(reader.result);
             localStorage.setItem('clientPrivateKey', reader.result);
-        }
+        };
     }
     
     return (
         <div className="App">
             <div>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" onBlur={changeUsername}/> <br />
+                <input type="text" id="username" onBlur={changeUsername}/> <br/>
             </div>
             <div>
                 <label htmlFor="username">Public</label>
                 <input id="publicKey" type="file" onChange={readPublicKey}/> <br/>
                 <label htmlFor="username">private</label>
-                <input id="privateKey" type="file" onChange={readPrivateKey} />
+                <input id="privateKey" type="file" onChange={readPrivateKey}/>
             </div>
             
             <button id="bnt1" onClick={ping}>Ping</button>
